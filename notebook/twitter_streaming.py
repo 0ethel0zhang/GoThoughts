35119a0ae45ed7b15dd24b788106983cc4fe5dea
Ethel Zhang <ethel.s.zhang@ibm.com>
2019-05-24 15:23:07 -0400

    Empty data directories

--- notebook/twitter_streaming.py
+++ notebook/twitter_streaming.py
@@ -1,33 +1,47 @@
 #Import the necessary methods from tweepy library
+import tweepy
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+#Get access keys
+import sys
+sys.path.insert(0, '../secret/')
+import access
 
 #Variables that contains the user credentials to access Twitter API
-access_token = "ENTER YOUR ACCESS TOKEN"
-access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
-consumer_key = "ENTER YOUR API KEY"
-consumer_secret = "ENTER YOUR API SECRET"
+access_token = access.access_token #"ENTER YOUR ACCESS TOKEN"
+access_token_secret = access.access_token_secret #"ENTER YOUR ACCESS TOKEN SECRET"
+consumer_key = access.consumer_key #"ENTER YOUR API KEY"
+consumer_secret = access.consumer_secret #"ENTER YOUR API SECRET"
 
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-        print data
+        print(data)
         return True
 
     def on_error(self, status):
-        print status
+        print(status)
 
+#Getting Access
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+try:
+    redirect_url = auth.get_authorization_url()
+except tweepy.TweepError:
+    print('Error! Failed to get request token.')
+
+api = tweepy.API(auth)
 
 if __name__ == '__main__':
 
     #This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    #auth = OAuthHandler(consumer_key, consumer_secret)
+    #auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['python', 'javascript', 'ruby'])
+    stream.filter(track=['GameofThrones'])
