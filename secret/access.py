9bce32bde2e84d3899e9a93ee1132bd9befebae8
Ethel Zhang <ethel.s.zhang@ibm.com>
2019-06-17 00:30:29 -0500

    fix .gitignore

--- secret/access.py
+++
@@ -1,4 +0,0 @@
-access_token = "226548987-vpqYkAgg2abGzAtTba2oR8U8Rj1XH8r2oVtL9Wm4"
-access_token_secret = "GctwPImOwmk0GmtAjOQvgt2nrNsp19xbCHTxEHxYG6sRh"
-consumer_key = "ptzqlAMF8SuYacPzBR5CnxozD"
-consumer_secret = "tORo0HAadkAdg0mldFGzOjz0mHEKdvdcOSWh7Icgf0TaXsg8mc"
